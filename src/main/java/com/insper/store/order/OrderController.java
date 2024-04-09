27882742759ef800cf636d1df41d9ab92554711f
thomaschiari <thomaschiari@gmail.com>
2024-04-08 21:17:04 -0300

    Update Controller

--- src/main/java/com/insper/store/order/OrderController.java
+++ src/main/java/com/insper/store/order/OrderController.java
@@ -13,25 +13,19 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface OrderController {
 
     @PostMapping("/orders")
-    public ResponseEntity<Order> create(
-        @RequestBody(required = true) Order in
-    );
-
-    @PutMapping("/orders/{id}")
-    public ResponseEntity<Order> update(
-        @PathVariable(required = true) String id,
-        @RequestBody(required = true) Order in
+    public ResponseEntity<OrderOut> create(
+        @RequestBody(required = true) OrderIn in
     );
 
     @GetMapping("/orders/{id}")
-    public ResponseEntity<Order> read(
+    public ResponseEntity<OrderOut> get(
         @PathVariable(required = true) String id
     );
 
-    @GetMapping("/orders")
-    public ResponseEntity<Order> read(
-        @RequestHeader(required = true, name = "id-user") String idUser,
-        @RequestHeader(required = true, name = "role-user") String roleUser
+    @PutMapping("/orders/{id}")
+    public ResponseEntity<OrderOut> update(
+        @PathVariable(required = true) String id,
+        @RequestBody(required = true) OrderIn in
     );
-    
+
 }
