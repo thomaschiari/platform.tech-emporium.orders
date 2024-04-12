766da5950c2ce873409bbf1fa6814eef754fa727
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-04-12 09:04:17 -0300

    fix: configs for running docker

--- src/main/java/com/insper/store/order/OrderController.java
+++ src/main/java/com/insper/store/order/OrderController.java
@@ -10,31 +10,26 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.RequestHeader;
 
-import io.swagger.annotations.ApiOperation;
 
 @FeignClient(name = "store-order")
 public interface OrderController {
 
-    @ApiOperation(value = "Create a new order")
     @PostMapping("/orders")
     public ResponseEntity<OrderOut> create(
         @RequestBody(required = true) OrderIn in
     );
 
-    @ApiOperation(value = "Get order by id")
     @GetMapping("/orders/{id}")
     public ResponseEntity<OrderOut> get(
         @PathVariable(required = true) String id
     );
 
-    @ApiOperation(value = "Update order by id")
     @PutMapping("/orders/{id}")
     public ResponseEntity<OrderOut> update(
         @PathVariable(required = true) String id,
         @RequestBody(required = true) OrderIn in
     );
 
-    @ApiOperation(value = "Delete order by id")
     @DeleteMapping("/orders/{id}")
     public ResponseEntity<Void> delete(
         @PathVariable(required = true) String id
